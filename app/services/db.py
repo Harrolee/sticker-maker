ea06fbadc389ceed93cce2420bd5f855cb3b6011
Lee Harrold <lhharrold@sep.com>
2024-11-10 09:08:15 -0500

    wild theory

--- app/services/db.py
+++ app/services/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
-from make_sticker.config import StickerConfig
+from ..make_sticker.config import StickerConfig
 
 config = StickerConfig()
 
