d8705a79bd2b7077598b9d318df3ea5e28e2cc16
Lee Harrold <lhharrold@sep.com>
2024-11-13 08:50:19 -0500

    minimal reproducible example

--- test_dir/test_app/main.py
+++ test_dir/test_app/main.py
@@ -1,38 +1,30 @@
-from contextlib import asynccontextmanager
-from dotenv import dotenv_values
+# from contextlib import asynccontextmanager
+# from dotenv import dotenv_values
 from fasthtml.common import *
-from app.make_sticker.config import StickerConfig
-# from app.services.db import DbClient
+from app.module_one.rugged import log_hi
+log_hi()
 
-@asynccontextmanager
-async def lifespan(app: FastHTML):
-    # Set up globally accessible singleton objects like db connection pool here
-    config = dotenv_values(dotenv_path="app/.env")
-    app.state.config = StickerConfig(config)
-    # app.state.db_client = DbClient(config)
-    yield
-    # Clean up globally accessible singleton objects here
-    # app.state.db_client.close()
+# @asynccontextmanager
+# async def lifespan(app: FastHTML):
+#     # Set up globally accessible singleton objects like db connection pool here
+#     config = dotenv_values(dotenv_path="app/.env")
+#     app.state.config = StickerConfig(config)
+#     # app.state.db_client = DbClient(config)
+#     yield
+#     # Clean up globally accessible singleton objects here
+#     # app.state.db_client.close()
 
 app,rt = fast_app()
 
-@rt('/dot-relative-import')
-def get():
-    from .make_sticker.config import StickerConfig
-    config = StickerConfig()
-    from .module_one.rugged import log_hi
-    return log_hi()
-
-
-@rt('/relative-import')
-def get():
-    from module_one.rugged import log_hi
-    return log_hi()
-
-
-@rt('/absolute-import')
+@rt('/test-app-import')
 def get():
     from test_app.module_one.rugged import log_hi
+    return log_hi()
+
+
+@rt('/app-import')
+def get():
+    from app.module_one.rugged import log_hi
     return log_hi()
 
 serve()
